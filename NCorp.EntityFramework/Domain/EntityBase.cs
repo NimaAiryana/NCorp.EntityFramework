68a4d816ae421ee63546e7e92c1537bafe3f627e
NimaAiryana <airyana.nima@gmail.com>
2021-08-15 22:26:15 +0430

    init

---
+++ NCorp.EntityFramework/Domain/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NCorp.EntityFramework.Domain
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public long CreatedAtUtc { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    }
+}
---
+++ NCorp.EntityFramework/Domain/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NCorp.EntityFramework.Domain
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        long CreatedAtUtc { get; set; }
+    }
+}
